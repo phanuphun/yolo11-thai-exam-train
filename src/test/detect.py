ece45ceb21d71a2f148fd8667008cca58d7e3aa8
first <parnuphun1598@gmail.com>
2025-12-23 04:21:12 +0700

    .

--- src/test/detect.py
+++ src/test/detect.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("best.pt")
+model = YOLO("src/models/best.pt")
 name_to_id = {v: k for k, v in model.names.items()}
 keep = [name_to_id["pb"], name_to_id["qt"], name_to_id["cg"], name_to_id["c"], name_to_id["img"]] 
 results = model.predict(
-    "test_data/vol2/page_002.png", 
+    "test_data/vol0/13.png", 
     classes=keep,
     conf=0.1,
     iou=0.45,
