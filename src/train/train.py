7309438207cd1e78409053b1d66ed476820f4242
first <parnuphun1598@gmail.com>
2025-12-22 19:30:53 +0700

    .

--- src/train/train.py
+++ src/train/train.py
@@ -1,14 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO("best.pt")  # load a pretrained YOLOv8n model
+model = YOLO("src/models/best.pt")
 
 train_results = model.train(
-    data="datasets/data.yaml",  
-    epochs=100, 
+    data="datasets/data.yaml",
+    epochs=100,
     imgsz=640,
     device="0",
     workers=0,
     val=True,
 )
-
 # metrics = model.val()
